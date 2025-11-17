9659e5919536b9b5e19989393bece14d18887c21
Mariano <marianogiardinamonti1@gmail.com>
2025-11-17 13:10:18 -0300

    Feat: endpoint get fuentes creado

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const tipografias = [
+  { "id": 1, "nombre": "Sansation" },
+  { "id": 2, "nombre": "Playfair Display" },
+  { "id": 3, "nombre": "Roboto" },
+  { "id": 4, "nombre": "Montserrat" }
+]
+
 app.get('/', (req, res) => {
   res.send('Hola soy juan:)');
+});
+
+app.get('/api/fonts', (req, res) => {
+  res.json(tipografias);
 });
 
 app.listen(port, () => {
